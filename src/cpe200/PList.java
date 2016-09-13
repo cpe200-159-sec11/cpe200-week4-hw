5c99208f9e377e6141b5943bad1a8d90187e4e7b
napornapon <na_hahaha@hotmail.com>
2016-09-13 21:29:14 +0700

    Update PList.java

--- src/cpe200/PList.java
+++ src/cpe200/PList.java
@@ -57,15 +57,24 @@ public class PList {
     }
 
     public boolean remove(Object data) {
-
         PNode tmp = head, tmp2;
-
         while (tmp != null) {
             if (tmp.data.equals(data)) {
-                // implement your code here!!!
-                // case 1: head of the list
-                // case 2: tail of the list
-                // case 3: somewhere in the middle
+                if(tmp.equals(head)) {
+                    popHead();
+                    return true;
+                }
+                else if(tmp.equals(tail)) {
+                    popTail();
+                    return true;
+                }
+                else {
+                    tmp2 = tmp.prev;
+                    tmp2.next = tmp.next;
+                    tmp.next.prev = tmp2;
+                    size--;
+                    return true;
+                }
             }
             tmp = tmp.next;
         }
@@ -73,16 +82,27 @@ public class PList {
     }
 
     public Object elementAt(int index) {
-        // implement your code here!!!
-        // what if index is not in between 0 to (size-1)
 
-        return null;
+        PNode tmp = head;
+        if(index>size || index<0) {
+            return null;
+        }
+        for(int i=0; i<index; i++) {
+            tmp = tmp.next;
+        }
+
+        return tmp.data;
     }
 
-    // rename the search method to "found(Object data)"
     public boolean found(Object data) {
-        // implement your code here!!!
+        PNode tmp=head;
 
+        while (tmp != null) {
+            if(tmp.data.equals(data)) {
+                return true;
+            }
+            tmp = tmp.next;
+        }
         return false;
     }
 
